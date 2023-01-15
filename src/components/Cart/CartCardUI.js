f9b7203a95f6d0c561aede0c0c50987e31ff30c4
afahnyupila03 <fulopila9@gmail.com>
2023-01-15 13:45:07 +0100

    update to cart

--- src/components/Cart/CartCardUI.js
+++ src/components/Cart/CartCardUI.js
@@ -1,4 +1,4 @@
-import { Card, Container, Button } from "react-bootstrap";
+import { Row, Col, Card, Container, Button } from "react-bootstrap";
 import { useState } from "react";
 import Wrapper from "../../Helpers/Wrapper";
 
@@ -23,9 +23,18 @@ const CartCardUI = (props) => {
                             {props.quantity} x {hover ? props.name : props.name.slice(0, 20) + '...'} 
                         </Card.Title>
                         
-                        <Button type='submit' className='m-auto align-self-center d-flex' variant='danger' onClick={() => props.onCartRemove(props.product) | props.onCartReduceCount(props.product) }>
-                                <i className="fa fa-trash m-auto align-self-center d-flex fa-2x" aria-hidden="true"></i>
-                            </Button>
+                        <Row>
+                            <Col>
+                                <Button type='submit' className='m-auto align-self-center d-flex' variant='danger' onClick={() => props.onCartRemove(props.product) | props.onCartReduceCount(props.product)}>
+                                    <i className="fa fa-trash m-auto align-self-center d-flex fa-2x" aria-hidden="true"></i>
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button type='submit' onClick={() => props.onCartClickAdd(props.product) | props.onCartCountClickAdd(props.product)} className='m-auto align-self-center d-flex' variant='danger'>
+                                    <i className="fa fa-shopping-cart fa-2x m-auto align-self-center d-flex" aria-hidden="true"></i>
+                                </Button>
+                            </Col>
+                        </Row>
                     </Card.Body>
                 </Card>
             </Container>
--- src/components/Wishlist/WishlistCartCardUI.js
+++ src/components/Wishlist/WishlistCartCardUI.js
@@ -22,18 +22,18 @@ const WishlistCartCardUI = (props) => {
                         <Card.Title className='mb-3' style={{fontSize: '1rem'}} >
                             { hover ? props.name : props.name.slice(0, 20) + '...'}
                         </Card.Title>
-                            <Row>
-                                <Col>
-                                    <Button type='submit' onClick={() => props.onWishlistRemove(props.product) | props.onReduceCountWishlist(props.product)} className='m-auto align-self-center d-flex' variant='danger'>
-                                        <i className="fa fa-trash fa-2x m-auto align-self-center d-flex" aria-hidden="true"></i>
-                                    </Button>
-                                </Col>
-                                <Col>
-                                    <Button type='submit' onClick={() => props.onCartClickAdd(props.product) | props.onCartCountClickAdd(props.product)} className='m-auto align-self-center d-flex' variant='danger'>
-                                        <i className="fa fa-shopping-cart fa-2x m-auto align-self-center d-flex" aria-hidden="true"></i>
-                                    </Button>
-                                </Col>
-                            </Row>
+                        <Row>
+                            <Col>
+                                <Button type='submit' onClick={() => props.onWishlistRemove(props.product) | props.onReduceCountWishlist(props.product)} className='m-auto align-self-center d-flex' variant='danger'>
+                                    <i className="fa fa-trash fa-2x m-auto align-self-center d-flex" aria-hidden="true"></i>
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button type='submit' onClick={() => props.onCartClickAdd(props.product) | props.onCartCountClickAdd(props.product)} className='m-auto align-self-center d-flex' variant='danger'>
+                                    <i className="fa fa-shopping-cart fa-2x m-auto align-self-center d-flex" aria-hidden="true"></i>
+                                </Button>
+                            </Col>
+                        </Row>
                                 
                                 
                     </Card.Body>
